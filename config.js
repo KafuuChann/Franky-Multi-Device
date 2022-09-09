9d68cb3e9b5929a768e0b32ed731c61c069a98cc
MaF Developer <98296475+MaFdeveloper@users.noreply.github.com>
2022-09-09 13:05:33 +0800

    Update config.js

--- config.js
+++ config.js
@@ -44,11 +44,11 @@ global.Intervalmsg = 1800 //detik
 
 //========Url Template Buttons=========//
 global.dtu = 'ɪɴꜱᴛᴀɢʀᴀᴍ'
-global.urlnya = "https://www.instagram.com/aguzfamilia"
+global.urlnya = "https://www.instagram.com/Juaan1991"
 
 //============= callButtons =============//
 global.dtc = 'ᴄᴀʟʟ ᴏᴡɴᴇʀ'
-global.phn = '+62 813-2017-0984'
+global.phn = '+62 8874-3500-141'
 
 //============= Games ================//
 global.benar = '_*Benar✅*_'
