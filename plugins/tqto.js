8f22eaef4414d10ca6c6397095c123532d94bb5c
MaF Developer <98296475+MaFdeveloper@users.noreply.github.com>
2022-09-09 13:01:37 +0800

    Update tqto.js

--- plugins/tqto.js
+++ plugins/tqto.js
@@ -6,27 +6,7 @@
 
 let handler = async (m, { conn, usedPrefix }) => {
     let tqto = `*BIG THANKS TO*
-
-Nurutomo: 
-https://github.com/Nurutomo
-Istikmal: 
-https://github.com/BochilGaming
-Ariffb: 
-https://github.com/Ariffb25
-Aguz Familia:
-https://github.com/FokusDotId
-Amelia Lisa:
-https://github.com/Ameliascrf
-Aniq12
-https://github.com/aniq12
-Ilman: 
-https://github.com/ilmanhdyt
-Amirul: 
-https://github.com/amiruldev20
-Irwan:
-https://github.com/irwanx
-Rasel:
-https://github.com/raselcomel
+gwehj
 
 \`\`\`${conn.user.name}\`\`\`
 `
